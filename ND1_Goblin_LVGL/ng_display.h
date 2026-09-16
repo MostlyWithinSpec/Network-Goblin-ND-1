e4d4ab6803e98aa82e78e7db961da5fbc5c0fc09
MostlyWithinSpec <308173363+MostlyWithinSpec@users.noreply.github.com>
2026-09-16 12:52:23 -0400

    Delete ND1_Goblin_LVGL directory
    Removing to add current build

--- ND1_Goblin_LVGL/ng_display.h
+++
@@ -1,6 +0,0 @@
-/* ng_display.h - LVGL + ST7796 + FT6336U */
-#pragma once
-#include <Arduino.h>
-void ngDisplayBegin();
-void ngDisplayTick();
-void ngBacklight(int percent);
--- ND1_Goblin_LVGL/ng_pairs.h
+++
@@ -1,9 +0,0 @@
-/* ng_pairs.h - per-pair cable status */
-#pragma once
-#include <Arduino.h>
-#include "ng_tdr.h"
-enum NgPairState { NGP_OK=0, NGP_OPEN, NGP_SHORT, NGP_NA };
-struct NgPair { const char* name; NgPairState state; float dist_ft; bool testable; };
-struct NgWiremap { NgPair p12, p36, p45, p78; bool linked; };
-void ngPairsRun(NgWiremap& out);
-const char* ngPairStateStr(NgPairState s);
